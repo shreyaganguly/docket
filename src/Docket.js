08802ec0f4ccc36c35e440204b88245a0c125aa7
Shreya Ganguly <shreya.ganguly99@gmail.com>
2017-11-09 16:26:36 +0530

    Added card based logic to add tasks

--- src/Docket.js
+++ src/Docket.js
@@ -5,36 +5,54 @@ import TextField from "material-ui/TextField";
 import ContentAdd from "material-ui/svg-icons/content/add";
 import FloatingActionButton from "material-ui/FloatingActionButton";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui/Card";
+import FlatButton from "material-ui/FlatButton";
 
 export default class Docket extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
-      input: "",
-      disabled: true
+      cards: []
     };
   }
-  handleChange(e) {
-    this.setState({ input: e.target.value });
+  handleChange(index, self, e) {
+    self.state.cards[index].input = e.target.value;
     if (e.target.value.length > 0) {
-      this.setState({ disabled: false });
+      self.state.cards[index].disabled = false;
     } else {
-      this.setState({ disabled: true });
+      self.state.cards[index].disabled = true;
     }
+    self.setState({ cards: self.state.cards });
   }
 
-  handleClick() {
-    this.state.items.unshift({
-      task: this.state.input,
+  handleClick(index, self, e) {
+    self.state.cards[index].items.unshift({
+      task: self.state.cards[index].input,
       decoration: { textDecoration: "none" },
       checked: false
     });
-    this.setState({ items: this.state.items, input: "", disabled: true });
+    self.state.cards[index].input = "";
+    self.setState({ cards: self.state.cards, disabled: true });
   }
-  handleAdd() {}
-  handleCheck(index, self, a) {
-    var item = self.state.items[index];
+
+  handleAdd() {
+    this.state.cards.unshift({
+      disabled: true,
+      input: "",
+      items: []
+    });
+    this.setState({ cards: this.state.cards, input: "", disabled: true });
+  }
+
+  handleCheck(cardIndex, itemIndex, self, a) {
+    var item = self.state.cards[cardIndex].items[itemIndex];
     if (item.checked === false) {
       item.checked = true;
       item.decoration = { textDecoration: "line-through" };
@@ -42,7 +60,15 @@ export default class Docket extends Component {
       item.checked = false;
       item.decoration = { textDecoration: "none" };
     }
-    self.setState({ items: self.state.items });
+    self.setState({ cards: self.state.cards });
+  }
+
+  handleDelete(index, self, a) {
+    console.log("Hello Hello");
+    console.log(index);
+    console.log(a);
+    delete self.state.cards[index];
+    self.setState({ cards: self.state.cards });
   }
 
   render() {
@@ -60,36 +86,64 @@ export default class Docket extends Component {
           >
             <ContentAdd />
           </FloatingActionButton>
-          <TextField
-            id="tasks"
-            value={this.state.input}
-            style={{ margin: 10 }}
-            floatingLabelText="Enter your task"
-            onChange={this.handleChange.bind(this)}
-          />
-          <RaisedButton
-            backgroundColor="#1e93f1"
-            label="Add this"
-            labelColor="#fff"
-            disabled={this.state.disabled}
-            disabledBackgroundColor="#a4a4a4"
-            onClick={this.handleClick.bind(this)}
-          />
-
-          <ul>
-            {this.state.items.map(function(item, i) {
-              return (
-                <div key={i}>
-                  <Checkbox
-                    label={item.task}
-                    labelStyle={item.decoration}
-                    checked={item.checked}
-                    onCheck={that.handleCheck.bind(this, i, that)}
-                  />
-                </div>
-              );
-            })}
-          </ul>
+          {this.state.cards.map(function(card, i) {
+            return (
+              <ul>
+                {card !== undefined && (
+                  <Card>
+                    <CardHeader
+                      title={`Docket #${i + 1}`}
+                      subtitle="Your Tasks"
+                    />
+                    <CardTitle title="Card title" subtitle="Card subtitle" />
+                    <CardText>
+                      {" "}
+                      <TextField
+                        id="tasks"
+                        value={card.input}
+                        style={{ margin: 10 }}
+                        floatingLabelText="Enter your task"
+                        onChange={that.handleChange.bind(this, i, that)}
+                      />
+                      <RaisedButton
+                        backgroundColor="#1e93f1"
+                        label="Add this"
+                        labelColor="#fff"
+                        disabled={card.disabled}
+                        disabledBackgroundColor="#a4a4a4"
+                        onClick={that.handleClick.bind(this, i, that)}
+                      />
+                      <ul>
+                        {card.items.map(function(item, j) {
+                          return (
+                            <div key={j}>
+                              <Checkbox
+                                label={item.task}
+                                labelStyle={item.decoration}
+                                checked={item.checked}
+                                onCheck={that.handleCheck.bind(
+                                  this,
+                                  i,
+                                  j,
+                                  that
+                                )}
+                              />
+                            </div>
+                          );
+                        })}
+                      </ul>
+                    </CardText>
+                    <CardActions>
+                      <FlatButton
+                        label="Delete"
+                        onClick={that.handleDelete.bind(this, i, that)}
+                      />
+                    </CardActions>
+                  </Card>
+                )}
+              </ul>
+            );
+          })}
         </div>
       </MuiThemeProvider>
     );
